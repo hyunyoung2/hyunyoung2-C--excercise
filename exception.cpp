79613faf3ed3ae7a7630d381cef2cad68df5167b
Lee <le32146@gamil.com>
2014-04-15 08:50:07 +0900

    start of exception

---
+++ exception.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void SimpleFunThree(void)
+{
+	try{
+	cout << "void SimpleFunThree(void)" << endl;
+	throw -1;
+	}
+	catch(char expn)
+	{
+		cout << "catch(char esf)" <<endl;
+	}
+}
+
+
+void SimpleFunTwo(void)
+{
+	cout << "void SimpleFunTwo(void)" << endl;
+	SimpleFunThree();
+}
+
+void SimpleFunOne(void)
+{
+	cout << "void SimpleFunOne(void)" << endl;
+	SimpleFunTwo();
+}
+
+int main()
+{
+	try
+	{
+		SimpleFunOne();
+	}
+	catch(long expn)
+	{
+		cout << "예외코드: " << expn << endl;
+	}
+
+	return 0;
+}
